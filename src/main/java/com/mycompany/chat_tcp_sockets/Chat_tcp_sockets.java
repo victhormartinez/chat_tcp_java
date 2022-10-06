db52f96909c02e69185be459f3793ec83017efe3
VICTOR MARTINEZ <martinez.ing21@gmail.com>
2022-10-06 11:35:28 -0500

    version 0.6 beta

---
+++ src/main/java/com/mycompany/chat_tcp_sockets/Chat_tcp_sockets.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.chat_tcp_sockets;
+
+/**
+ *
+ * @author AlguienSinAmor
+ */
+public class Chat_tcp_sockets {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
